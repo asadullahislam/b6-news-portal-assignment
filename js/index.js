320352623e18ab0d0215dc350bcd6bc4abeb193d
asadullahislam <asadullah0072002@gmail.com>
2022-09-03 23:40:35 +0600

    all news added

--- js/index.js
+++ js/index.js
@@ -6,14 +6,14 @@ const loadAllCategory = async () => {
     const data = await res.json();
 
     // console.log(data.data.news_category[0]);
-    const dataNews = data.data.news_category;
+    const categories = data.data.news_category;
 
 
     const allMenu = document.getElementById('all-menu');
 
-    for (const news of dataNews) {
+    for (const category of categories) {
         const a = document.createElement('all-menu');
-        a.innerHTML = ` <a onclick="loadCategoryDetail()" class="navbar-brand" href="#">${news.category_name}</a>`
+        a.innerHTML = ` <a onclick="loadCategoryDetail('${category.category_id}')" class="navbar-brand" href="#">${category.category_name}</a>`
         allMenu.appendChild(a);
 
         // console.log(news);
@@ -22,49 +22,60 @@ const loadAllCategory = async () => {
 }
 // const loadCategoryDetail = async () => {
 
-// const loadCategoryDetail = () => {
-//     const url = `https://openapi.programming-hero.com/api/news/category/01`
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(data => displayCategoryDetail(data.data[0]))
-// }
-// const displayCategoryDetail = category => {
-//     console.log(category);
+const loadCategoryDetail = async (categoryId) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`
+    const res = await fetch(url)
+    const data = await res.json();
+    // console.log()
+    const allNews = data.data;
+    console.log(allNews);
+    const newsDetailsContainer = document.getElementById('news-details');
+    newsDetailsContainer.innerHTML = ``;
+    for (const news of allNews) {
+        console.log(news)
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('row');
+        newDiv.innerHTML = `
+                    <div class="col-3">
+                    <img class="img-fluid" src="${news.thumbnail_url}" alt="">
+                </div>
+                <div class="col-9">
+                    <div>
+                        <h2>${news.title}</h2>
+                        <p>${news.details.slice(0, 300)}...</p>
+                    </div>
+        
+                    <div class="d-flex justify-content-between">
+                        <div>
+                        <img class="img-fluid author-img rounded-circle" src="${news.author.img}" alt="">
+                        </div>
+                         <div>
+                        
+                            <h4>${news.author.name ? news.author.name : 'Not Available'}</h4>
+                            <p>${news.author.published_date.slice(0, 10)}</p>
+                        </div>
+        
+                        <p><i class="fa-regular fa-eye"></i> ${news.total_view ? news.total_view : '0'}</p>
+                        <p></i><i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-regular fa-star-half-stroke"></i>
+        
+                        </p>
+        
+                        <p><i class="fa-solid fa-arrow-right"></i></p>
+                    </div>
+        
+        
+        
+                </div>
+        `
+        newsDetailsContainer.appendChild(newDiv);
+    }
 
-//     const categoryDetail = document.getElementById('news-details');
-//     categoryDetail.innerHTML =
+}
 
-//         `
-//             <div class="col-3">
-//             <img class="img-fluid" src="" alt="">
-//         </div>
-//         <div class="col-9 d-flex flex-column">
-//             <div>
-//                 <h6></h6>
-//                 <p></p>
-//             </div>
-
-//             <div class="d-flex justify-content-between">
-//                 <div>
-//                     <h4 ></h4>
-//                     <p></p>
-//                 </div>
-
-//                 <p><i class="fa-regular fa-eye"></i></i></p>
-//                 <p></i><i class="fa-solid fa-star"></i>
-//                     <i class="fa-solid fa-star"></i>
-//                     <i class="fa-solid fa-star"></i>
-//                     <i class="fa-solid fa-star"></i>
-//                     <i class="fa-regular fa-star-half-stroke"></i>
-
-//                 </p>
-
-//                 <p><i class="fa-solid fa-arrow-right"></i></p>
-//             </div>
-
-
-
-//         </div>
 
 //     `
 
@@ -72,66 +83,6 @@ const loadAllCategory = async () => {
 
 // }
 
-//     const url = `https://openapi.programming-hero.com/api/news/category/01`;
-//     const res = await fetch(url)
-//     const data = await res.json();
-//     // console.log(data.data);
-//     dataDetails = data.data[0];
-//     console.log(dataDetails);
-
-
-
-
-//     const newsDetail = document.getElementById('news-details');
-//     // for (const news of dataDetails) {
-//     //     console.log(news);
-
-
-// newsDetail.innerHTML = `
-//     <div class="col-3">
-//     <img class="img-fluid" src="${news.image_url}" alt="">
-// </div>
-// <div class="col-9 d-flex flex-column">
-//     <div>
-//         <h6>${news.title}</h6>
-//         <p>${news.details}</p>
-//     </div>
-
-//     <div class="d-flex justify-content-between">
-//         <div>
-//             <h4 >${news.author.name}</h4>
-//             <p>${news.author.published_date}</p>
-//         </div>
-
-//         <p><i class="fa-regular fa-eye"></i></i> ${news.total_view}</p>
-//         <p></i><i class="fa-solid fa-star"></i>
-//             <i class="fa-solid fa-star"></i>
-//             <i class="fa-solid fa-star"></i>
-//             <i class="fa-solid fa-star"></i>
-//             <i class="fa-regular fa-star-half-stroke"></i>
-
-//         </p>
-
-//         <p><i class="fa-solid fa-arrow-right"></i></p>
-//     </div>
-
-
-
-// </div>
-
-//             `
-
-
-
-
-
-
-//     // }
-
-
-
-
-// }
 
 
 // loadCategoryDetail();
