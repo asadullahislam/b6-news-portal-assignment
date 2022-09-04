1ab5ed382f547139e983b88f2182672fac91b324
asadullahislam <asadullah0072002@gmail.com>
2022-09-04 19:40:54 +0600

    before add sort

--- js/index.js
+++ js/index.js
@@ -29,15 +29,21 @@ const loadCategoryDetail = async (categoryId) => {
     // console.log()
     const allNews = data.data;
     console.log(allNews);
+
+
+    const newsNumberContainer = document.getElementById('news-number');
+    newsNumberContainer.innerText = allNews.length;
     const newsDetailsContainer = document.getElementById('news-details');
     newsDetailsContainer.innerHTML = ``;
+    const modalContainer = document.getElementById('modal-container');
+    modalContainer.innerHTML = ``;
     for (const news of allNews) {
         console.log(news)
         const newDiv = document.createElement('div');
         newDiv.classList.add('row');
         newDiv.classList.add('border');
         newDiv.innerHTML = `
-                    <div class="col-3">
+                <div class="col-3">
                     <img class="img-fluid" src="${news.thumbnail_url}" alt="">
                 </div>
                 <div class="col-9">
@@ -49,11 +55,11 @@ const loadCategoryDetail = async (categoryId) => {
                     <div class="d-flex justify-content-between">
                         <div>
                         <img class="img-fluid author-img rounded-circle" src="${news.author.img}" alt="">
-                        </div>
-                         <div>
+                    </div>
+                        <div>
                         
                             <h4>${news.author.name ? news.author.name : 'Not Available'}</h4>
-                            <p>${news.author.published_date.slice(0, 10) ? news.author.published_date : 'Not Available'}</p>
+                            <p>${news.author.published_date ? news.author.published_date.slice(0, 10) : 'Not Available'}</p>
                         </div>
         
                         <p><i class="fa-regular fa-eye"></i> ${news.total_view ? news.total_view : '0'}</p>
@@ -64,17 +70,54 @@ const loadCategoryDetail = async (categoryId) => {
                             <i class="fa-regular fa-star-half-stroke"></i>
         
                         </p>
-        
-                        <button onclick="loadCategoryDetail('')"  class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newDetailModal><i class="fa-solid fa-arrow-right"></i></button>
-                    </div>
-        
-        
+
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal${news._id}">
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </button>
                 </div>
         `
         newsDetailsContainer.appendChild(newDiv);
+
+        // modal body 
+        const modalContainer = document.getElementById('modal-container');
+        const modalDiv = document.createElement('div');
+        modalDiv.innerHTML = `
+        
+        <div class="modal fade" id="modal${news._id}" tabindex="-1" aria-labelledby="modal${news._id}Label"
+        aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modal${news._id}Label">${news.title}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div id="modal-detail" class="modal-body">
+                   <div>  <img class="img-fluid" src="${news.image_url}" alt=""></div>
+                   <p>${news.details}</p>
+                </div>
+                <div>
+                <div class="d-flex justify-content-between">
+                <div>
+                <img class="img-fluid author-img rounded-circle" src="${news.author.img}" alt="">
+            </div>
+                <div>
+                
+                    <h4>${news.author.name ? news.author.name : 'Not Available'}</h4>
+                    <p>${news.author.published_date ? news.author.published_date.slice(0, 10) : 'Not Available'}</p>
+                </div>
+ 
+                <p><i class="fa-regular fa-eye"></i> ${news.total_view ? news.total_view : '0'}</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+        
+        `
+        modalContainer.appendChild(modalDiv);
     }
-
-
 
 }
 
@@ -87,7 +130,7 @@ const loadCategoryDetail = async (categoryId) => {
 
 
 
-// loadCategoryDetail();
+loadCategoryDetail('08');
 
 
 
